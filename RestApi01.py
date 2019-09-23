77278db941573f307c230db7d3d432b917fa7ddd
hafidzadnan <hafidz.adnan@gmail.com>
2019-09-23 11:54:17 +0700

    Test Commit penambahan di RestApi01

--- RestApi01.py
+++ RestApi01.py
@@ -1,3 +1,10 @@
 pipenv install requests
 import requests
 requests.get('https://api.github.com')
+
+from flask import Flask
+from flask_restful import Resource, reqparse ,Api
+
+TGS = Flask(__name__)
+api = Api(TGS)
+
